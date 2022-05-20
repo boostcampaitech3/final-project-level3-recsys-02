ede1bcba6c5f9fd7df06c4ec8d2f99cb15485d4f
Allen Clarence <zombiedelah@gmail.com>
2022-05-20 19:37:53 +0900

    added a generator wrapper class for the sse implementation

--- modules/events.py
+++ modules/events.py
@@ -1,9 +1,29 @@
+import asyncio
 from datetime import datetime
+import time
+from typing import Callable
+
+
+class Generator:
+    @staticmethod
+    async def timeBound(timeout: float, interval: float, function: Callable):
+        timeElapsed = 0.0
+        timeStarted = time.perf_counter()
+        while timeElapsed < timeout:
+            await asyncio.sleep(interval)
+            timeElapsed = time.perf_counter() - timeStarted
+            yield await function()
+
+    @staticmethod
+    async def iterBound(iterations: int, interval: float, function: Callable):
+        for rep in range(iterations):
+            await asyncio.sleep(interval)
+            yield await function()
 
 
 class ServerLog:
     @staticmethod
-    def formatter(message):
+    def formatter(message: str):
         """
         Pod console 에 메시지 표시할 때 포맷해주는 wrapper function
         :param message: log string
