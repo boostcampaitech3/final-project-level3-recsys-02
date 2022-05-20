86bd022d4a0658d4b4911e522f14c38b70ac3789
Allen Clarence <zombiedelah@gmail.com>
2022-05-20 19:36:35 +0900

    added a rate limiter and an SSE implementation

--- api-server.py
+++ api-server.py
@@ -1,18 +1,22 @@
-import asyncio
 from fastapi import FastAPI
 from fastapi import Request
 from modules.broker import Broker
-from modules.events import ServerLog
-from modules.orm import UserRequest
+from modules.events import Generator, ServerLog
+
+# Rate Limiting for QOS
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
+# Server Sent Events Implementation
+from sse_starlette.sse import EventSourceResponse
 
 """
-sse => Server Side Event 
+sse => Server Side Event
+Generator.timeBound | Generator.iterBound 
+Both are static wrapper functions
 
-slowapi => Throttling (rate limit 설정) 요청 수 제한을 구현하기 위해 in-memory storage 에 요청을 캐싱해야 합니다.
+slowapi => Throttling (rate limit 설정) 요청 수 제한을 위해 in-memory storage 에 요청을 캐싱하는 구현체
 rate limits string format
 [count] [per|/] [n (optional)] [second|minute|hour|day|month|year]
 출처 : "https://limits.readthedocs.io/en/stable/quickstart.html#rate-limit-string-notation"
@@ -28,35 +32,24 @@ logger = ServerLog()
 broker = Broker(HOST, logger)
 
 
+async def sayHello():
+    return 'hello, world!'
+
+
+@app.get('/')
+@limiter.limit('3/second')
+async def root(request: Request):
+    return EventSourceResponse(Generator.timeBound(5.0, 1.0, sayHello))
+
+
 @app.on_event('startup')
 async def init():
     await broker.connect()
     await broker.createStream('inference', ['pub', 'sub'])
 
 
-@app.post('/inference/pub')
+@app.post('/inference/place_v1')
 @limiter.limit('3/second')
 async def requestInference(request: Request):
-    await broker.createStream('inference', [''])
+    # return EventSourceResponse()
     return {}
-
-
-@app.get('/inference/sub')
-@limiter.limit('3/second')
-async def requestResult(request: Request):
-    '''
-    for key, value in request.headers.items():
-        print('{key} | {keyType} : {value} | {valueType}'.format(
-            key=key,
-            keyType=type(key),
-            value=value,
-            valueType=type(value),
-            )
-        )
-    '''
-    payload = {
-        'id': 'abcd',
-        'lon': 35.0,
-        'lat': 38.0,
-    }
-    return payload
