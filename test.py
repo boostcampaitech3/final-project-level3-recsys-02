fd8a3ddccf61e8c285fa716d8d8e66c2d6ed1157
ParkByungYoon <21800271@handong.edu>
2022-05-25 07:09:59 +0000

    inference logic

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-import json
 import requests
 
 
 payload = {
-    'lat': 37.532600,
-    'lon': 127.024612,
+    'lon': 127.0554836,
+    'lat': 37.2863722,
+    'placeID': 10780,
 }
 
-response = requests.post('http://{host}/inference/{ch}'.format(host='127.0.0.1:8000', ch='dhl'), json=payload)
+response = requests.post('http://{host}/inference/{ch}'.format(host='127.0.0.1:0', ch='byp'), json=payload)
 response = dict(response.json())
-data = json.loads(response['response'])
-print(data)
+print(response)
